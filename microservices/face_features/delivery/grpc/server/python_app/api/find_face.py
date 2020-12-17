649d97f0468c316cb0b6f00a9e6813574dea4564
nathanKith <ntolparov@gmail.com>
2020-12-17 23:25:59 +0300

    ^update mask logic

--- microservices/face_features/delivery/grpc/server/python_app/api/find_face.py
+++ microservices/face_features/delivery/grpc/server/python_app/api/find_face.py
@@ -1,30 +1,37 @@
 import cv2
 from PIL import Image
 
-def find_face():
-    face_cascade = cv2.CascadeClassifier('frontalface_default.xml')
 
-    img = cv2.imread('test.png')
+def get_mask_path(mask_name):
+    return 'masks/' + mask_name + '.png'
 
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+def get_photo_mask_name(path, mask):
+    return path + '_' + mask + '.png'
 
-    # for (x, y, w, h) in faces:
-    #     cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-    face = Image.open('test.png')
 
-    x, y, w, h = faces[0]
-    mask = Image.open('mask.png')
-    resize_mask = mask.resize((w, h))
-
-    face.paste(resize_mask, (x, y),  resize_mask)
-    face.save('img_with_mask.png')
-
-    #cv2.imshow('img', face)
-    #cv2.waitKey()
+def find_faces(path):
+    face_cascade = cv2.CascadeClassifier('cascade/frontalface_default.xml')
+    img = cv2.imread(path)
+    faces = face_cascade.detectMultiScale(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), 1.1, 4)
 
     return faces
 
 
-find_face()
+def overlay_mask(path, mask):
+    faces = find_faces(path)
+
+    photo = Image.open(path)
+
+    for face in faces:
+        x, y, w, h = face
+
+        mask_path = get_mask_path(mask)
+        mask_photo = Image.open(mask_path).resize((w, h))
+
+        photo.paste(mask_photo, (x, y),  mask_photo)
+
+    photo_path = get_photo_mask_name(path, mask)
+    photo.save(photo_path)
+
+    return photo, photo_path
